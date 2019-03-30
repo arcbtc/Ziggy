6622d8a64a1d34f3e41db190eff1d3b071deddd5
Arc <33088785+arcbtc@users.noreply.github.com>
2019-03-30 15:56:57 +0000

    Update main.ino

--- writedata/main.ino
+++ writedata/main.ino
@@ -1,1 +1,110 @@
+//Note: You will need to strip out the "-" from the OpenNode API key
 
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         22        
+#define SS_PIN          5         
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
+ 
+void setup() {
+  Serial.begin(9600);        
+  SPI.begin();               
+  mfrc522.PCD_Init();       
+  Serial.println(F("Write personal data on a MIFARE PICC "));
+}
+
+void loop() {
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.print(F("Card UID:"));    //Dump UID
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.print(F(" PICC type: "));   // Dump PICC type
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+
+  byte buffer[34];
+  byte block;
+  MFRC522::StatusCode status;
+  byte len;
+
+  Serial.setTimeout(20000L) ;     // wait until 20 seconds for input from serial
+  // Ask personal data: Family name
+  Serial.println(F("Type LN Node Address, followed by #"));
+  len = Serial.readBytesUntil('#', (char *) buffer, 32) ; // read family name from serial
+  
+  byte buffer2[16];
+  
+  
+  //PRINT first bit
+  for (uint8_t i = 0; i < 16; i++) {
+    Serial.write(buffer[i] );
+  }
+
+   //PRINT first bit
+  for (uint8_t i = 16; i < 32; i++) {
+    buffer2[i-16] = buffer[i];
+    Serial.write(buffer2[i-16] );
+  }
+
+//-------------------------------------------WRITE 1st 16 BYTE PART OF API KEY
+
+  block = 1;
+  //Serial.println(F("Authenticating using key A..."));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("PCD_Authenticate() success: "));
+
+  // Write block
+  status = mfrc522.MIFARE_Write(block, buffer, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("MIFARE_Write() to block 1 success: "));
+
+//-------------------------------------------WRITE 2nd 16 BYTE PART OF API KEY
+
+  block = 2;
+  //Serial.println(F("Authenticating using key A..."));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  // Write block
+  status = mfrc522.MIFARE_Write(block, buffer2, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("MIFARE_Write() to block 2 success: "));
+
+  Serial.println(" ");
+  mfrc522.PICC_HaltA(); // Halt PICC
+  mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
+
+}
